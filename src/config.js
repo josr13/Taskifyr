4439fb068fdc906be530ea905a2741af2649fc57
josr13 <joseph-l-r-r@hotmail.com>
2023-06-30 15:49:19 -0500

    Add JWT access token and bcrypt password hash

--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT;
+export const TOKEN_SECRET = process.env.TOKEN_SECRET;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,34 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
+
   try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
-    res.json(userSaved);
+
+    const token = await createAccessToken({ id: userSaved._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({ message: error.message });
   }
 };
 
